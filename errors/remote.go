4caed0d042bc068a8e002fad8eb571ca24e40939
Raphael C. Almeida <almeida.raphael.c@gmail.com>
2021-04-29 01:32:34 -0300

    Added Is error interface method to Remote errors

--- errors/remote.go
+++ errors/remote.go
@@ -1,5 +1,6 @@
 package errors
 
+import "errors"
 
 // Remote remote error struct on go error
 type Remote struct {
@@ -9,4 +10,14 @@ type Remote struct {
 // Error compliance with error interface
 func (e *Remote) Error() string {
 	return e.Err.Error()
+}
+
+// Is method to comply with new errors functions
+func (e *Remote) Is(target error) bool {
+	tar, ok := target.(*Remote)
+	if !ok {
+		return false
+	}
+
+	return errors.Is(e.Err, tar.Err) || tar.Err == nil
 }

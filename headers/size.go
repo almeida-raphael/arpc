d66aa85987133cbf3edc31ea0b2263c137fc53b3
Raphael C. Almeida <almeida.raphael.c@gmail.com>
2021-06-16 15:46:37 -0300

    - Removed unused function

--- headers/size.go
+++
@@ -1,9 +0,0 @@
-package headers
-
-// Size gets the size of the header and ignores errors
-func Size()uint64{
-	dummyHeader := Header{}
-	var size int
-	size, _ = dummyHeader.MarshalLen()
-	return uint64(size)
-}
